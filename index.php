fed39ce728a022d33f54accd7d36012b1875ee4f
s4r44d43 <sararez@live.it>
2024-04-15 16:20:08 +0200

    [end] first part

--- index.php
+++ index.php
@@ -52,6 +52,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.css' integrity='sha512-U9Y1sGB3sLIpZm3ePcrKbXVhXlnQNcuwGQJ2WjPjnp6XHqVTdgIlbaDzJXJIAuCTp3y22t+nhI4B88F/5ldjFA==' crossorigin='anonymous'/>
+
   <link rel="stylesheet" href="./style/main.css">
 
   <title>HOTEL</title>
@@ -60,15 +62,28 @@
 
   <section class="container">
 
-    <ul>
+    <h1 class="text-center">INFO</h1>
+
+    <div>
       <?php foreach($hotels as $oro): ?>
-        <?php foreach($oro as $key => $item): ?>
-        <li>
-        <?php echo $key ?> : <?php echo $item ?> 
-        </li>
-        <?php endforeach ?>
+        <h2 class="mt-4"> 
+          HOTEL <i class="fa-solid fa-hotel"></i> :
+        </h2>
+        <ul>
+          <?php foreach($oro as $key => $item): ?>
+            <li class="my-4 d-flex">
+              <div class="name">
+                <?php echo $key ?>
+              </div>
+              <div class="in">
+                <span> : </span>
+                <?php echo $item ?> 
+              </div>
+            </li>
+          <?php endforeach ?>
+        </ul>
       <?php endforeach ?>
-    </ul>
+    </div>
 
   </section>
 
